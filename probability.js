28f40393862695bbc6c2706450a2e7522ab334df
Paulo Henrique Tavares de Andrade <ph.and@Hotmail.com>
2021-06-07 17:12:14 -0300

    turning code into functions

--- probability.js
+++ probability.js
@@ -4,25 +4,51 @@
 // https://www.gamblingsites.com/blog/are-blackjack-games-with-2-to-1-payouts-as-amazing-as-they-seem/
 // https://mindfuckmatica.wordpress.com/tag/blackjack/
 
+// Advanced implementation: https://www.youtube.com/watch?v=fvSaTohme-g
+
 // Betway Classic Blackjack
 const deck = ["Ace", "Ace", "Ace", "Ace", 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "Jack", "Jack", "Jack", "Jack", "Queen", "Queen", "Queen", "Queen", "King", "King", "King", "King"];
 const deck8 = [];
 let dealersHand = [];
 let openingCard;
 let hand = [];
+let splitHand = [];
 let drawCard;
 let handValue = [];
 let handValueSum = 0;
+let splitHandValue = [];
+let splitHandValueSum = 0;
 let stand = false;
 let double = false;
 let split = false;
-let blackjack = 0;
+let blackJack = 0;
 
 // Creates an array with 8 decks
 for(let i = 0; i < 1; i++){
     deck.forEach(card => {
         deck8.push(card);
     });
+}
+
+function drawCards(cardsNumber = 1){
+    for(let i = 0; i < cardsNumber; i++){
+        drawCard = Math.abs(Math.round(Math.random()* deck8.length - 1));
+        console.log(`Array index to draw card: ${drawCard}`);
+        hand.push(deck8[drawCard]);
+        console.log(`My hand: ${hand}`);
+        deck8.splice(drawCard, 1);
+        console.log(`Deck remainder: ${deck8}`);
+        console.log(`Deck length: ${deck8.length}`);
+        if (split === true){
+            drawCard = Math.abs(Math.round(Math.random()* deck8.length - 1));
+            console.log(`Array index to draw card: ${drawCard}`);
+            splitHand.push(deck8[drawCard]);
+            console.log(`My second hand: ${splitHand}`);
+            deck8.splice(drawCard, 1);
+            console.log(`Deck remainder: ${deck8}`);
+            console.log(`Deck length: ${deck8.length}`);
+        }
+    }
 }
 
 // Dealer first round
@@ -41,32 +67,17 @@ for(let i = 0; i < 2; i++){
 }
 
 // My first round
-for(let i = 0; i < 2; i++){
-    drawCard = Math.abs(Math.round(Math.random()* deck8.length - 1));
-    console.log(`Array index to draw card: ${drawCard}`);
-    hand.push(deck8[drawCard]);
-    console.log(`My hand: ${hand}`);
-    deck8.splice(drawCard, 1);
-    console.log(`Deck remainder: ${deck8}`);
-    console.log(`Deck length: ${deck8.length}`);
-}
+drawCards(2);
 
-//hand.splice(0, 1, 10);
-//hand.splice(1, 1, 10);
+
+hand.splice(0, 1, 'Ace');
+hand.splice(1, 1, 'Ace');
 console.log(hand);
-console.log(hand[0]);
 console.log(openingCard);
 
 // Points conversion
-for(let i = 0; i < 2; i++){
-    if (hand[i] === 'Jack' || hand[i] === 'Queen' || hand[i] === 'King'){
-        handValue.push(10);
-    }  else {
-        handValue.push(hand[i]);
-    }
-    handValueSum += handValue[i];
-} 
-console.log(handValueSum);
+
+handSum(hand, 2);
 
 if (openingCard === 'Jack' || openingCard === 'Queen' || openingCard === 'King'){
     openingCardValue = 10;
@@ -79,147 +90,204 @@ if (openingCard === 'Jack' || openingCard === 'Queen' || openingCard === 'King')
 console.log(openingCardValue);
 
 //Rules
-if (hand[0] === hand[1]){
-     if (hand[0] === 'Ace'){
-        console.log(`You have ${hand} so SPLIT!`);
-        split = true;
-     } else if (hand[0] === 2 || hand[0] === 3){
-        if (openingCardValue >= 2 && openingCardValue <= 7){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
-            split = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-     } else if (hand[0] === 4){
-        if (openingCardValue >= 4 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
-            split = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-     } else if (hand[0] === 5){
-        if (openingCardValue >= 2 && openingCardValue <= 9){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
-            double = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-     } else if (hand[0] === 6){
-        if (openingCardValue >= 4 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
-            stand = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-     } else if (hand[0] === 7 || hand[0] === 8){
-        if (openingCardValue >= 4 && openingCardValue <= 7){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
-            split = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-     }  else if (hand[0] === 9){
-        if (openingCardValue >= 2 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
-            split = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
-            stand = true;
-        }
-     } else { //10, Jack, Queen and King -> Need to confirm if, fx King and Jack would be considered equal as well
-        console.log(`You have ${hand} so STAND!`);
-        stand = true;
-     } 
-} else if (hand[0] === "Ace" || hand[1] === "Ace"){
-    if (hand[0] === 2 || hand[1] === 2 || hand[0] === 3 || hand[1] === 3){
-        if (openingCardValue >= 5 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
-            double = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-    } else if (hand[0] === 4 || hand[1] === 4 || hand[0] === 5 || hand[1] === 5){
-        if (openingCardValue >= 4 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
-            double = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-    } else if (hand[0] === 6 || hand[1] === 6){
-        if (openingCardValue >= 3 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
-            double = true;
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        }
-    } else if (hand[0] === 7 || hand[1] === 7){
-        if (openingCardValue >= 3 && openingCardValue <= 6){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
-            double = true;
-        } else if (openingCardValue >= 9 && openingCardValue <= 10 || openingCardValue === 'Ace'){
-            console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
-        } else {
-            console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
-            stand = true;
-        }
-    } else if (hand[0] === 8 || hand[1] === 8 || hand[0] === 9 || hand[1] === 9){
-        console.log(`You have ${hand} so STAND!`);
-        stand = true;
-    } else {
-        console.log(`You have ${hand} so BLACKJACK!!!`);
-        stand = true;
-        blackjack++;
-    }
-} else {
-    if (handValueSum <= 8){
-        console.log(`You have a sum of ${handValueSum} so DRAW A CARD!`);
-    } else if (handValueSum === 9){
-        if (openingCardValue >= 3 && openingCardValue <= 6){
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
-            double = true;
-        } else {
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
-        }
-    } else if (handValueSum === 10){
-        if (openingCardValue >= 2 && openingCardValue <= 9){
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
-            double = true;
-        } else {
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
-        }
-    } else if (handValueSum === 11){
-        if (openingCardValue >= 2 && openingCardValue <= 10){
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
-            double = true;
-        } else {
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
-        }
-    } else if (handValueSum >= 12 && handValueSum <= 16){
-        if (openingCardValue >= 4 && openingCardValue <= 6){
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so STAND`);
-            stand = true;
-        } else {
-            console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
-        }
-    } else if (handValueSum >= 17 && handValueSum <= 20){
-        console.log(`You have have a sum of ${handValueSum} so STAND!`);
-        stand = true;
-    } else {
-        console.log(`You have have a sum of ${handValueSum} so BLACKJACK!!!`);
-        stand = true;
-        blackjack++;
-    }
-};
+function rules (hand){
+    if (hand[0] === hand[1] || split === true){
+        if (hand[0] === 'Ace'){
+            if (split === true){
+                console.log(`You have ${hand} and just splitted aces so you had to STAND.`);
+            } else{
+                console.log(`You have ${hand} so SPLIT!`);
+                split = true;
+            }
+        } else if (hand[0] === 2 || hand[0] === 3){
+           if (openingCardValue >= 2 && openingCardValue <= 7){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
+               split = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+        } else if (hand[0] === 4){
+           if (openingCardValue >= 4 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
+               split = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+        } else if (hand[0] === 5){
+           if (openingCardValue >= 2 && openingCardValue <= 9){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
+               double = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+        } else if (hand[0] === 6){
+           if (openingCardValue >= 4 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
+               stand = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+        } else if (hand[0] === 7 || hand[0] === 8){          //always split 8s?
+           if (openingCardValue >= 4 && openingCardValue <= 7){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
+               split = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+        }  else if (hand[0] === 9){
+           if (openingCardValue >= 2 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so SPLIT!`);
+               split = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
+               stand = true;
+           }
+        } else { //10, Jack, Queen and King -> Need to confirm if, fx King and Jack would be considered equal as well
+           console.log(`You have ${hand} so STAND!`);
+           stand = true;
+        } 
+   } else if (hand[0] === "Ace" || hand[1] === "Ace"){
+       if (hand[0] === 2 || hand[1] === 2 || hand[0] === 3 || hand[1] === 3){
+           if (openingCardValue >= 5 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
+               double = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+       } else if (hand[0] === 4 || hand[1] === 4 || hand[0] === 5 || hand[1] === 5){
+           if (openingCardValue >= 4 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
+               double = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+       } else if (hand[0] === 6 || hand[1] === 6){
+           if (openingCardValue >= 3 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
+               double = true;
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           }
+       } else if (hand[0] === 7 || hand[1] === 7){
+           if (openingCardValue >= 3 && openingCardValue <= 6){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DOUBLE!`);
+               double = true;
+           } else if (openingCardValue >= 9 && openingCardValue <= 10 || openingCardValue === 'Ace'){
+               console.log(`You have ${hand} and the dealer has ${openingCard} so DRAW A CARD!`);
+           } else {
+               console.log(`You have ${hand} and the dealer has ${openingCard} so STAND!`);
+               stand = true;
+           }
+       } else if (hand[0] === 8 || hand[1] === 8 || hand[0] === 9 || hand[1] === 9){
+           console.log(`You have ${hand} so STAND!`);
+           stand = true;
+       } else {
+           console.log(`You have ${hand} so BLACKJACK!!!`);
+           stand = true;
+           blackJack++;
+       }
+   } else {
+       if (handValueSum <= 8){
+           console.log(`You have a sum of ${handValueSum} so DRAW A CARD!`);
+       } else if (handValueSum === 9){
+           if (openingCardValue >= 3 && openingCardValue <= 6){
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
+               double = true;
+           } else {
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
+           }
+       } else if (handValueSum === 10){
+           if (openingCardValue >= 2 && openingCardValue <= 9){
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
+               double = true;
+           } else {
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
+           }
+       } else if (handValueSum === 11){
+           if (openingCardValue >= 2 && openingCardValue <= 10){
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DOUBLE`);
+               double = true;
+           } else {
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
+           }
+       } else if (handValueSum >= 12 && handValueSum <= 16){
+           if (openingCardValue >= 4 && openingCardValue <= 6){
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so STAND`);
+               stand = true;
+           } else {
+               console.log(`You have a sum of ${handValueSum} and the dealer has ${openingCard} so DRAW`);
+           }
+       } else if (handValueSum >= 17 && handValueSum <= 20){
+           console.log(`You have have a sum of ${handValueSum} so STAND!`);
+           stand = true;
+       } else {
+           console.log(`You have have a sum of ${handValueSum} so BLACKJACK!!!`);
+           stand = true;
+           blackJack++;
+       }
+   };
+}
 
+
+rules(hand);
+
+// Second round
 if (stand === false) {
     if (split === true){
+        splitHand.push(hand[0]);
+        hand.pop();
+        drawCards();
+        handSum(hand, hand.length);
+        //handSum(splitHand, 1);
+        console.log(hand);
+        console.log(splitHand);
+        rules(hand);
+        rules(splitHand);
+        if (hand[0] === 'Ace'){
+            stand = true;
+            split = false;
+        } else if ((hand[0] === 2 || hand[0] === 3)){
 
+        }
     } else if (double === true){
-
-    } else {
         
+    } else {
+
     }
 }
+
+function handSum(hand, cardsNumber, handValueSum = 0, aceCounter = 0){
+    console.log(aceCounter);
+    console.log(hand);
+    console.log(handValueSum);
+    for (i = 0; i < cardsNumber; i++){
+        if (hand[i] === 'Jack' || hand[i] === 'Queen' || hand[i] === 'King'){
+            handValue.push(10);
+            handValueSum += 10;
+        }  else if (hand[i] === 'Ace'){
+           aceCounter++;
+           console.log(aceCounter);
+        } else {
+            handValue.push(hand[i]);
+            handValueSum += hand[i];
+        }
+    }
+    for (i = 0; i < aceCounter; i++){
+        console.log(handValueSum);
+        if (handValueSum + 11 > 21){
+            handValueSum++;
+        } else {
+            handValueSum += 11;
+        }
+    }
+    console.log(aceCounter);
+    console.log(handValueSum);
+}
+
+
+
+
+
 
 
 
